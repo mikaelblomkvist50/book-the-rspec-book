df2d839adca5a25cb287e33d0847617115432e05
mikaelblomkvist50 <mikaelblomkvist50@gmail.com>
2018-05-30 12:41:38 +1000

    Add.

--- GettingStartedWithRSpecAndCucumber/ch4-automating-features-with-cucumber/features/step_definitions/codebreaker_steps.rb
+++ GettingStartedWithRSpecAndCucumber/ch4-automating-features-with-cucumber/features/step_definitions/codebreaker_steps.rb
@@ -1,7 +1,26 @@
+class FakeOutput
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def fake_output
+  @fake_output ||= FakeOutput.new
+end
+
 Given("I am not yet playing") do
 
 end
 
 When("I start a new game") do
-  Codebreker::Game.new.start
+  game = Codebreaker::Game.new(fake_output)
+  game.start
+end
+
+Then("I should see {string}") do |string|
+  fake_output.messages.should include(string)
 end
