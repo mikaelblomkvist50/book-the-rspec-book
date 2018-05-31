e30131612bb72fe0a2b71e91cebc79b7d694150d
mikaelblomkvist50 <mikaelblomkvist50@gmail.com>
2018-05-31 14:19:03 +1000

    Add.

--- GettingStartedWithRSpecAndCucumber/ch5-describing-code-with-rspec/lib/codebreaker/game.rb
+++ GettingStartedWithRSpecAndCucumber/ch5-describing-code-with-rspec/lib/codebreaker/game.rb
@@ -1,12 +1,14 @@
 module Codebreaker
   class Game
-    def initialize(output)
-      @output = output
+    def initialize(output) #output=> #<IO:<STDOUT>>
+      binding.pry
+      @output = output #@output=> #<IO:<STDOUT>>
+      binding.pry
     end
 
     def start
-      @output.puts 'Welcome to Codebreaker!'
-      @output.puts 'Enter guess:'
+      @output.puts 'Welcome to Codebreaker!' #=> STDOUT.puts 'Welcome to Codebraeker!'
+      @output.puts 'Enter guess:' #=> STDOUT.puts 'Enter guess:'
     end
   end
 end
